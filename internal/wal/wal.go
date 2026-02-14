1eb5db49ab4123d76a8563393304b3f3bf8551cc
n1kolapetrovic <dzonipetrovic2006@gmail.com>
2026-02-14 15:39:21 +0100

    Started Wal

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,12 +1,97 @@
 package wal
 
-import "kv-engine/internal/model"
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+	"os"
+)
 
-// TODO: implementirati segmentirani WAL
-type WAL struct{}
+const (
+	OpDelete byte = 0
+	OpPut    byte = 1
+)
 
-func New() *WAL { return &WAL{} }
+type FragmentType byte
 
-func (w *WAL) Append(_ model.Record) error { return nil }
+const (
+	FragmentFull   FragmentType = 0 // ceo zapis staje u jedan fragment
+	FragmentFirst  FragmentType = 1
+	FragmentMiddle FragmentType = 2
+	FragmentLast   FragmentType = 3
+)
 
-func (w *WAL) Replay(_ func(model.Record) error) error { return nil }
+type WALFragment struct {
+	Type    FragmentType
+	DataLen uint32
+	Data    []byte
+}
+
+func (f *WALFragment) NewWALFregment(Type FragmentType, DataLen uint32, Data []byte) *WALFragment {
+	return &WALFragment{
+		Type:    Type,
+		DataLen: DataLen,
+		Data:    Data,
+	}
+}
+
+type WALRecord struct {
+	CRC32     uint32
+	Seq       uint64
+	ExpiresAt uint64
+	OpType    byte // 0 = DELETE, 1 = PUT
+	KeyLen    uint32
+	ValueLen  uint32
+	Key       []byte
+	Value     []byte
+}
+
+// Binarizuje Wal zapis
+func (r *WALRecord) Serialize() []byte {
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.LittleEndian, r.CRC32)
+	binary.Write(buffer, binary.LittleEndian, r.Seq)
+	binary.Write(buffer, binary.LittleEndian, r.ExpiresAt)
+	binary.Write(buffer, binary.LittleEndian, r.OpType)
+	binary.Write(buffer, binary.LittleEndian, r.KeyLen)
+	binary.Write(buffer, binary.LittleEndian, r.ValueLen)
+	buffer.Write(r.Key)
+	buffer.Write(r.Value)
+	return buffer.Bytes()
+}
+
+// Konstruktor za ceo WAL zapis
+func NewWALRecord(seq uint64, expiresAt uint64, opType byte, key []byte, value []byte) *WALRecord {
+	wal := WALRecord{
+		CRC32:     0, // Racuna se kada se wal upisuje na disk
+		Seq:       seq,
+		ExpiresAt: expiresAt,
+		OpType:    opType,
+		KeyLen:    uint32(len(key)),
+		ValueLen:  uint32(len(value)),
+		Key:       key,
+		Value:     value,
+	}
+	// Izracunavanje CRC32
+	serialized := wal.Serialize()
+	wal.CRC32 = crc32.ChecksumIEEE(serialized[4:])
+	return &wal
+}
+
+type WALSegmentHeader struct {
+	Magic         [4]byte
+	BlockSize     int
+	SegmentBlocks int
+}
+
+type WALSegment struct {
+	File     *os.File
+	FilePath string
+}
+
+type WALManager struct {
+	DirPath        string
+	MaxSegmentSize int64
+	CurrentSegment *WALSegment
+	SegmentID      int
+}
